efae11b19d9deeda5203cfe466de6bc48e8f69f3
alizarazot <alizarazot@ufpso.edu.co>
2025-10-25 03:15:20 -0500

    Add Index

--- src/main/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplication.java
+++ src/main/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplication.java
@@ -2,12 +2,19 @@ package io.github.alizarazot.lasllavesdetucasa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LasLlavesDeTuCasaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LasLlavesDeTuCasaApplication.class, args);
 	}
 
+    @GetMapping("/")
+    public String index() {
+        return "<h1>Hello World!</h1>";
+    }
 }
