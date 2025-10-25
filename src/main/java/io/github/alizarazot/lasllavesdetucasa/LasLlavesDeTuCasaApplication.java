36fe798480f86269b621ac2f05da5c85355124b9
alizarazot <alizarazot@ufpso.edu.co>
2025-10-25 02:53:25 -0500

    Generate project with Spring Initializr

---
+++ src/main/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplication.java
@@ -0,0 +1,13 @@
+package io.github.alizarazot.lasllavesdetucasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasLlavesDeTuCasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasLlavesDeTuCasaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alizarazot.lasllavesdetucasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasLlavesDeTuCasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
