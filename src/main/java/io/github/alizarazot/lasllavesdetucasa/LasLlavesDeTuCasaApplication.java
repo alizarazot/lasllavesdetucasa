4f1c8956596b8cfb4dc891e6daf26f725acbc54c
alizarazot <alizarazot@ufpso.edu.co>
2025-12-11 00:16:26 -0500

    Add /home redirect

--- src/main/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplication.java
+++ src/main/java/io/github/alizarazot/lasllavesdetucasa/LasLlavesDeTuCasaApplication.java
@@ -1,7 +1,10 @@
 package io.github.alizarazot.lasllavesdetucasa;
 
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -10,5 +13,10 @@ public class LasLlavesDeTuCasaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LasLlavesDeTuCasaApplication.class, args);
+  }
+
+  @GetMapping("/home")
+  public void redirectHome(HttpServletResponse response) throws IOException {
+    response.sendRedirect("/");
   }
 }
